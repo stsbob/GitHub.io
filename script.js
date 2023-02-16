1f4b13e1e26a04fc93be24fab2e47242f05b5afb
stsbob <92600649+stsbob@users.noreply.github.com>
2023-02-16 16:31:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.querySelector('button').onclic = myClick;
 function myClick(){
 let a = document.querySelector('.i').value;
 console.log(a);//выводим в консоль значение
-
+document.getElementById("demo").innerHTML = a;
 }
